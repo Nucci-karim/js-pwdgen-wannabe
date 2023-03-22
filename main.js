9a7724836ac690a4591c7183587935e3d63de4d4
Nucci-karim <nuccikarim@gmail.com>
2023-03-22 16:48:27 +0100

    esercizio corretto

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const nome =  prompt("come ti chiami?")
 const cognome = prompt("come fai di cogome?")
 const colorepreferito = prompt("qual'è il tuo colore preferito?")
+const numero = 21 
 
 document.getElementById('frase').innerHTML = `<p>Ciao mi chiamo ${cognome} ${nome} ed il mio colore preferito è il ${colorepreferito}</p>`
 
-document.writeln("<p>"+ nome + cognome + colorepreferito +"21</p>")
+document.getElementById('frase').innerHTML = `${nome + cognome + colorepreferito + numero}`
